611bfdeb27eadf48baefd20204d703b9888c5cc6
larry <gudrun>
2020-04-04 14:18:47 +0200

    profiling and misc

--- run-td-gammon.scm
+++ run-td-gammon.scm
@@ -1,3 +1,4 @@
+(use-modules (statprof))
 
 (import (srfi srfi-1) (ice-9 match) (srfi srfi-8) (srfi srfi-9))
 (import (ffi cblas))
@@ -12,7 +13,9 @@
   (let* ((wnet (make-net))
          (bnet (make-net))
          (measure #f)
-         (episodes #f))
+         (episodes #f)
+         ; ---- debug stuff ----
+         (profiling #f))
     (do ((args (command-line) (cdr args)))
         ((eq? args '()))
       (format #t "  arg: ~s~%" (car args))
@@ -24,6 +27,10 @@
           (set! wnet #:late))
       (if (string-contains (car args) "--early")
           (set! wnet #:early))
+      (if (string-contains (car args) "--swap")
+          (let ((tmp wnet))
+            (set! wnet bnet)
+            (set! bnet tmp)))
       (if (string-contains (car args) "--wnet=")
           (set! wnet (file-load-net (substring (car args) 7) #t)))
       (if (string-contains (car args) "--bnet=")
@@ -31,11 +38,18 @@
       (if (string-contains (car args) "--measure=")
           (set! measure (substring (car args) 10)))
       (if (string-contains (car args) "--episodes=")
-          (set! episodes (string->number (substring (car args) 11)))))
-    (cond
-     (measure
-      (run-tdgammon-measure measure #:episodes episodes))
-     (else
-      (run-tdgammon wnet bnet #:save #t)))))
+          (set! episodes (string->number (substring (car args) 11))))
+      (if (string-contains (car args) "--profiling")
+          (set! profiling #t)))
+
+    (let ((thunk (lambda ()
+                   (cond
+                    (measure
+                     (run-tdgammon-measure measure #:episodes episodes))
+                    (else
+                     (run-tdgammon wnet bnet #:save #t #:episodes episodes))))))
+      (if profiling
+          (statprof thunk)
+          (thunk)))))
 
 (main)
